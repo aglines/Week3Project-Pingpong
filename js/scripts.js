4bd3ca6cb4b86088a999befdaee8145e2493da8b
Andrew Glines <aglines@gmail.com>
2017-05-05 09:21:04 -0700

    Add if/elseif logic to pingPong that converts multiples of 3, 5, and 15 to their correct output

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,32 @@
-var outputArray = [];
+var temp = "";
 
 // BACKEND LOGIC
 var pingPong = function(userInput) {
+  var outputArray = [];
   //ensure input is a number, if not return "invalid"
     // come back to this if need
   var number = parseInt(userInput);
-  for (i=1; i<= number; i+=1) {
+
+for (i=1; i<= number; i+=1) {
+  if (i%15 === 0) {
+    temp = "ping-pong";
+    outputArray.push(temp);
+  }
+  else if (i%3 === 0) {
+    temp = "ping";
+    outputArray.push(temp);
+  }
+  else if (i%5===0) {
+    temp = "pong";
+    outputArray.push(temp);
+  }
+  else {
     outputArray.push(i);
   }
-  return outputArray;
+}; // FOR LOOP
+return outputArray;
 
 }; // FUNCTION CLOSE
-
-
 
 // FRONTEND LOGIC
 $(document).ready(function() {
