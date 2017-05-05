ce14bf367fbed8ca1f12779fdb462739b33f3d62
Andrew Glines <aglines@gmail.com>
2017-05-05 10:48:23 -0700

    Add logic to ensure input is a number datatype, as well as an integer

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,34 @@
-var temp = "";
-
 // BACKEND LOGIC
 var pingPong = function(userInput) {
   var outputArray = [];
-  //ensure input is a number, if not return "invalid"
-    // come back to this if need
-  var number = parseInt(userInput);
+  //ensure input is a number datatype, and an integer
+  var number = Math.floor(Number(userInput));
+  if ( isNaN(number) ) {
+    return result = "Invalid entry; please use only numbers.";
 
-for (i=1; i<= number; i+=1) {
-  if (i%15 === 0) {
-    temp = "ping-pong";
-    outputArray.push(temp);
-  }
-  else if (i%3 === 0) {
-    temp = "ping";
-    outputArray.push(temp);
-  }
-  else if (i%5===0) {
-    temp = "pong";
-    outputArray.push(temp);
   }
   else {
-    outputArray.push(i);
-  }
-}; // FOR LOOP
-return outputArray;
 
-}; // FUNCTION CLOSE
+  for (i=1; i<= number; i+=1) {
+    if (i%15 === 0) {
+      temp = "ping-pong";
+      outputArray.push(temp);
+    }
+    else if (i%3 === 0) {
+      temp = "ping";
+      outputArray.push(temp);
+    }
+    else if (i%5===0) {
+      temp = "pong";
+      outputArray.push(temp);
+    }
+    else {
+      outputArray.push(i);
+    }
+  }; // FOR LOOP
+  return outputArray;
+} // ELSE
+}; // FUNCTION PINGPONG
 
 // FRONTEND LOGIC
 $(document).ready(function() {
